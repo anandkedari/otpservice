43faa0f32acd7f181d1e494debe17bf0302c7109
Anand Kedari <anandkedari@yahoo.com>
2022-05-20 12:10:41 +0530

    Anand: Adding mandatory field validation and filteration logic

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "encoding/json"
     "github.com/gorilla/mux"
     "io/ioutil"
+    "strings"
 )
 
 type Message struct{
@@ -14,24 +15,16 @@ type Message struct{
  SMSRecepientNumber string `json:"smsrecepient"`
 }
 
+type ErrorMessage struct{
+    ErrorCode string `json:"error_code"`
+    ErrorDescription string `json:"error_desc"`
+}
+
 var Messages []Message
+var errMessage ErrorMessage
 
 func home(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "OTP Service running")
-}
-
-func handleRequests() {
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/", home)
-
-    //Get SMS
-    myRouter.HandleFunc("/sms", returnSMSBody).Methods("GET")
-    myRouter.HandleFunc("/sms/{SMSRecepientNumber}", returnSingleSMSBody).Methods("GET")
-    //Post new SMS
-    myRouter.HandleFunc("/sms", addNewMessage).Methods("POST")
-    //Post new SMS
-    myRouter.HandleFunc("/sms/{SMSRecepientNumber}", deleteMessage).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
 func main() {
@@ -42,6 +35,21 @@ func main() {
     handleRequests()
 }
 
+func handleRequests() {
+    myRouter := mux.NewRouter().StrictSlash(true)
+    myRouter.HandleFunc("/", home)
+
+    //Get SMS
+    myRouter.HandleFunc("/sms", returnSMSBody).Methods("GET")
+    myRouter.HandleFunc("/sms1", returnSMSBody).Methods("GET")
+    myRouter.HandleFunc("/sms/{SMSRecepientNumber}", returnSingleSMSBody).Methods("GET")
+    //Post new SMS
+    myRouter.HandleFunc("/sms", addNewMessage).Methods("POST")
+    //Post new SMS
+    myRouter.HandleFunc("/sms/{SMSRecepientNumber}", deleteMessage).Methods("DELETE")
+    log.Fatal(http.ListenAndServe(":8000", myRouter))
+}
+
 func addNewMessage(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     // get the body of our POST request
@@ -49,12 +57,18 @@ func addNewMessage(w http.ResponseWriter, r *http.Request) {
     var message Message 
     json.Unmarshal(reqBody, &message) //unmarshal and save request in message variable
 
-    //deletes existing object from Messages array before adding new object for specified mobile number
-    deleteExistingMessage(message)
+    removeWhiteSpaces(&message);
+    if(validateMessage(message) && filteredMessage(message)){
+        //deletes existing object from Messages array before adding new object for specified mobile number
+        deleteExistingMessage(message)
 
-    // Add new essage to Messages array
-    Messages = append(Messages, message)
-    json.NewEncoder(w).Encode(message)
+        // Add new message to Messages array
+        Messages = append(Messages, message)
+        json.NewEncoder(w).Encode(message)
+    } else {
+        w.WriteHeader(400) // Return 400 Bad Request.
+        json.NewEncoder(w).Encode(errMessage)
+    }
 }
 
 func deleteExistingMessage(mes Message) {
@@ -96,4 +110,28 @@ func returnSingleSMSBody(w http.ResponseWriter, r *http.Request) {
             json.NewEncoder(w).Encode(currentmessage)
         }
     }
+}
+
+func validateMessage(mes Message) bool{
+    if ( (mes.SMSBody == "") || (mes.SMSSenderNumber == "")  || (mes.SMSRecepientNumber == "") ){
+        errMessage.ErrorCode = "VALIDATION_FAILURE"
+        errMessage.ErrorDescription = "Request body validation failed, check if all mandatory data is available"
+        return false    
+    }
+    return true
+}
+
+func filteredMessage(mes Message) bool{
+    if (strings.Contains(strings.ToLower(mes.SMSBody),strings.ToLower("OTP")) && strings.Contains(strings.ToLower(mes.SMSSenderNumber),strings.ToLower("IDFC"))){
+        return true    
+    }
+    errMessage.ErrorCode = "FILTERATION_FAILURE"
+    errMessage.ErrorDescription = "Request doesn't satisfy filter criteria(OTP & IDFC)"
+    return false
+}
+
+func removeWhiteSpaces(mes *Message){
+    mes.SMSBody = strings.TrimSpace(mes.SMSBody)
+    mes.SMSSenderNumber = strings.TrimSpace(mes.SMSSenderNumber)
+    mes.SMSRecepientNumber = strings.TrimSpace(mes.SMSRecepientNumber)
 }
